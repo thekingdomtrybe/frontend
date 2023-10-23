600eabb20781c6cd77ae66e1ac20f5d6ba315c08
Precious Betine <preciousbetine@gmail.com>
2023-10-23 07:09:08 +0100

    Setup jest for testing

--- src/components/SiteHeader/SiteHeader.test.js
+++ src/components/SiteHeader/SiteHeader.test.js
@@ -1,8 +1,17 @@
+/**
+ * @jest-environment jsdom
+*/
+
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import SiteHeader from './SiteHeader.jsx';
 
 it('renders correctly', () => {
-  const component = renderer.create(<SiteHeader />);
+  const component = renderer.create(
+    <BrowserRouter>
+      <SiteHeader />
+    </BrowserRouter>,
+  );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 
--- src/components/SiteLogo/SiteLogo.test.js
+++ src/components/SiteLogo/SiteLogo.test.js
@@ -1,8 +1,17 @@
+/**
+ * @jest-environment jsdom
+*/
+
 import renderer from 'react-test-renderer';
+import { BrowserRouter } from 'react-router-dom';
 import SiteLogo from './SiteLogo.jsx';
 
 it('renders correctly', () => {
-  const component = renderer.create(<SiteLogo />);
+  const component = renderer.create(
+    <BrowserRouter>
+      <SiteLogo />
+    </BrowserRouter>,
+  );
   const tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
